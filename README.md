3a2dd2ce00eb73ae7d6270990bfc456c8f92b660
Zach Bloss <zacharybloss@gmail.com>
2021-09-30 22:51:49 -0400

    add additional workflow to reply to users seeking to purchase NFTs
    rename workflow that posts my NFTs
    
    add description of workflows to readme
    
    add wallets to readme

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Python scripts for promoting NFTs to Social Media websits
 
 [![NFT Posting](https://github.com/zbloss/nft-promotion-bot/actions/workflows/sample.yml/badge.svg)](https://github.com/zbloss/nft-promotion-bot/actions/workflows/sample.yml)
+
+## nft_post
+
+This workflow runs every 12 hours. It loads my NFT collection from Opensea and filters down to the assets that I still own. It then downloads an image representing the NFT, and uploads it to twitter creating an advertising tweet.
+
+## nft_reply
+
+This workflow runs every hour. It checks for a mix of the 50 most popular and recent messages where twitter users are seeking to purchase other NFTs. It then replies to each of those with a link to my provided collection.
+
+### Wallets
+
+| _    | Wallet Address                                             |
+|------|------------------------------------------------------------|
+| ETH  | 0x9c847ce765a48701fba267366eefa12606780d72                 |
+| ADA  | addr1v9u4693xhh5eq5gnye4spaq3p8gqqjrwnykgwdlfhe0zajs5u453c |
+| BTC  | 3PL6n2vXTtg6sv3Nft8FqKbEaKNNEUva9g                         |
+| AAVE | 0x5F48a5739694140826A347f7b48f90c8463Bb2Bd                 |
---
+++ src/find_nft_buyers.py
@@ -0,0 +1,46 @@
+import json
+import hydra
+import shutil
+import tweepy
+import random
+import logging
+import requests
+from omegaconf import DictConfig
+from opensea import OpenseaAPI
+
+
+@hydra.main(config_path="../configs", config_name="base_config")
+def find_buyers(cfg: DictConfig) -> None:
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger(__name__)
+ 
+    api_key = cfg.twitter.api_key
+    api_secret = cfg.twitter.api_secret
+    access_token = cfg.twitter.access_token
+    access_token_secret = cfg.twitter.access_token_secret
+
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    logger.info('twitter client authenticated')
+
+    search_results = api.search_tweets(
+        q="drop your unsold nft",
+        result_type="mixed",
+        count=20
+    )
+    for tweet_number, result in enumerate(search_results):
+        tweet_details = result._json
+        tweet_id = tweet_details['id_str']
+        api.update_status(
+            status=f"https://opensea.io/collection/{cfg.opensea.collection}",
+            in_reply_to_status_id=tweet_id,
+            auto_populate_reply_metadata=True
+        )
+
+        logger.info(f'tweet posted: {tweet_number}')
+
+
+if __name__ == "__main__":
+    find_buyers()
